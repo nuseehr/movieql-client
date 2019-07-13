3c93de37a96f86f43ecb745660bbb034d30a79f6
Lee Sun Hong <nuseehr@naver.com>
2019-07-13 18:49:19 +0900

    Add movie detail function

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,35 @@
 import React from "react";
+import Movie from "./Movie";
 import { Query } from "react-apollo";
 import { HOME_PAGE } from "./queries";
+import styled from "styled-components";
+
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 0.7fr);
+  flex-wrap: wrap;
+  justify-items: center;
+`;
 
 const Home = () => (
-  <Query query={HOME_PAGE}>
-    {({ loading, data, error }) => {
-      if (loading) return "LOADING. . .";
-      if (error) return "SOMETHING HAPPENED";
-      console.log(data);
-
-      return data.movies.map(movie => (
-        <h2 key={movie.id}>
-          {movie.title} / {movie.rating}{" "}
-        </h2>
-      ));
-    }}
-  </Query>
+  <Container>
+    <Query query={HOME_PAGE}>
+      {({ loading, data, error }) => {
+        if (loading) return "LOADING. . .";
+        if (error) return "ERROR";
+        return data.movies.map(movie => (
+          <Movie
+            id={movie.id}
+            key={movie.id}
+            poster={movie.medium_cover_image}
+            title={movie.title}
+            rating={movie.rating}
+          />
+        ));
+      }}
+    </Query>
+  </Container>
 );
 
 export default Home;
--- src/queries.js
+++ src/queries.js
@@ -1,12 +1,31 @@
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 
-export const HOME_PAGE = gql `
-query {
-  movies(limit:50, rating: 8) {
-    id
-    title
-    rating
-    genres
+export const HOME_PAGE = gql`
+  query {
+    movies(limit: 50, rating: 8) {
+      id
+      title
+      rating
+      medium_cover_image
+    }
   }
-}
+`;
+
+export const MOVIE_DETAILS = gql`
+  query getMovieDetails($movieId: Int!) {
+    movie(id: $movieId) {
+      medium_cover_image
+      title
+      rating
+      description_intro
+      language
+      genres
+    }
+    suggestions(id: $movieId) {
+      id
+      title
+      rating
+      medium_cover_image
+    }
+  }
 `;
